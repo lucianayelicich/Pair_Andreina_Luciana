f33bac159b5b3d5eeb79922801ff781c1e84055b
Luciana Yelicich <yelicichluciana@gmail.com>
2025-06-21 15:20:23 +0200

    ejercicios join II terminados

--- ejercicios_join_2.sql
+++ ejercicios_join_2.sql
@@ -64,8 +64,54 @@ FROM
 JOIN
     Employees AS e2
 ON
-    e1.ReportsTo = e2.ReportsTo
-WHERE
-    e1.employee_number <> e2.employee_number;
+    e1.ReportsTo = e2.EmployeeID;
+    
+
+SELECT EmployeeID, FirstName, LastName, City
+FROM Employees
+WHERE ReportsTo IS NULL;
+
+/* El equipo de marketing necesita una lista con todas las categorías de productos, incluso si no tienen productos asociados. Queremos 
+obtener el nombre de la categoría y el nombre de los productos dentro de cada categoría. Podriamos usar un RIGTH JOIN con 'categories'?, 
+usemos tambien la tabla 'products'.*/
+
+SELECT c.CategoryID, c.CategoryName, p.ProductName
+FROM Categories AS c
+LEFT JOIN Products AS p
+ON c.CategoryID = p.CategoryID;
+
+SELECT c.CategoryID, c.CategoryName, p.ProductName
+FROM Products AS p
+RIGHT JOIN  Categories AS c
+ON c.CategoryID = p.CategoryID;
+
+-- Se puede usar un RIGHT JOIN comenzanso desde Products, pero el objetivo es listar todas las categorías, así que es más natural y claro 
+-- comenzar desde la tabla Categories.LEFT JOIN garantiza que se muestren todas las categorías, y si no tienen productos, el campo 
+-- ProductName será NULL.
+
+/* Desde el equipo de ventas nos piden obtener una lista de todos los pedidos junto con los datos de las empresas clientes. Sin embargo, 
+hay algunos pedidos que pueden no tener un cliente asignado. Necesitamos asegurarnos de incluir todos los pedidos, incluso si no tienen 
+cliente registrado*/
+
+SELECT o.OrderID, o.CustomerID, c.CompanyName, c.Country
+FROM Orders AS o
+LEFT JOIN Customers AS c
+ON c.CustomerID = o.CustomerID;
+
+/*El equipo de Recursos Humanos quiere saber qué empleadas han gestionado pedidos y cuáles no. Queremos obtener una lista con todas las 
+empleadas y, si han gestionado pedidos, mostrar los detalles del pedido.*/
+
+SELECT o.EmployeeID, o.OrderID, o.CustomerID, o.OrderDate
+FROM Employees AS e
+LEFT JOIN Orders AS o
+ON o.EmployeeID = e.EmployeeID;
+
+/* Desde el área de logística nos piden una lista de todos los transportistas (shippers) y los pedidos que han enviado. Queremos 
+asegurarnos de incluir todos los transportistas, incluso si no han enviado pedidos.*/
+
+SELECT s.ShipperID, s.CompanyName, o.OrderID, o.OrderDate
+FROM Shippers AS s
+LEFT JOIN Orders AS o
+ON s.ShipperID = o.ShipVia;
 
 
