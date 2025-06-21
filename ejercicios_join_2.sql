394ae05c90c9b86279452fe16de4d28a8329e847
Luciana Yelicich <yelicichluciana@gmail.com>
2025-06-21 12:55:39 +0200

    ejercicios join II

---
+++ ejercicios_join_2.sql
@@ -0,0 +1,71 @@
+USE tienda;
+
+-- ejercicio 1 Selecciona el ID, nombre, apellidos de todas las empleadas y el ID de cada cliente asociado a ellas (si es que lo tienen).
+
+SELECT e.employee_number, e.first_name, e.last_name, c.customer_number
+FROM employees AS e
+LEFT JOIN customers AS C
+ON e.employee_number = c.sales_rep_employee_number
+WHERE c.customer_number IS NOT NULL;
+
+-- ejercicio 2 Selecciona el ID de todos los clientes, y el nombre, apellidos de las empleadas que llevan sus pedidos (si es que las hay).
+
+SELECT c.customer_number, e.first_name, e.last_name
+FROM customers AS c
+LEFT JOIN employees AS e
+ON e.employee_number = c.sales_rep_employee_number
+WHERE e.first_name IS NOT NULL AND e.last_name IS NOT NULL;
+
+-- ejercicio 3 Selecciona el ID, nombre, apellidos de las empleadas, para aquellas con más de 8 clientes, usando LEFT JOIN.
+
+SELECT e.first_name, e.last_name, e.employee_number
+FROM employees AS e
+LEFT JOIN customers AS c
+ON e.employee_number = c.sales_rep_employee_number
+GROUP BY e.first_name, e.last_name, e.employee_number
+HAVING COUNT(c.customer_number>8);
+
+-- ejercicio 4 Selecciona el ID, nombre, apellidos de las empleadas, para aquellas con más de 8 clientes, usando RIGHT JOIN.
+
+SELECT e.first_name, e.last_name, e.employee_number
+FROM employees AS e
+RIGHT JOIN customers AS c
+ON e.employee_number = c.sales_rep_employee_number
+GROUP BY e.first_name, e.last_name, e.employee_number
+HAVING COUNT(c.customer_number>8);
+
+-- ejercicio 5 Selecciona el nombre y apellidos de las empleadas que tienen clientes de más de un país, usando LEFT JOIN.
+
+SELECT e.first_name, e.last_name
+FROM employees AS e
+LEFT JOIN customers AS c
+ON e.employee_number = c.sales_rep_employee_number
+GROUP BY e.first_name, e.last_name
+HAVING COUNT(DISTINCT c.country>1);
+
+/* EJERCICIOS EXTRAS */
+
+USE northwind;
+
+-- Ejercicio 1 Empleadas que sean de la misma ciudad:
+
+/*Desde recursos humanos nos piden realizar una consulta que muestre por pantalla los datos de todas las empleadas y sus supervisoras. 
+Concretamente nos piden: la ubicación, nombre, y apellido tanto de las empleadas como de las jefas. Investiga el resultado, 
+¿sabes decir quién es el director?*/
+
+SELECT
+    e1.EmployeeID AS employee_ID,
+    e1.FirstName AS employee1_first_name, 
+    e1.LastName AS employee1_last_name,   
+    e1.City AS employee_city,
+    e1.ReportsTo AS employee_reports
+FROM
+    Employees AS e1
+JOIN
+    Employees AS e2
+ON
+    e1.ReportsTo = e2.ReportsTo
+WHERE
+    e1.employee_number <> e2.employee_number;
+
+
